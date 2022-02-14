2185cca9740fde0d46bd1c76b30cff1827864b0a
BenjaminGWilson <benjamin.g.wilson@gmail.com>
2022-02-14 17:45:19 +0000

    CRUD operations added

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
 //imports
 require("./db/connection");
 const express = require("express");
+const movieRouter = require("./movies/movieRoutes");
 
 //other globals
 const app = express();
 const port = process.env.PORT || 5001;
 
 app.use(express.json());
+app.use(movieRouter);
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
